d4ead56e8ffcc6bfe870527aaa055ca528beeabf
Chirath Jayawardena <chirath@calcey.com>
2023-12-13 10:53:32 +0530

    init

---
+++ ExternalApi/Program.cs
@@ -0,0 +1,39 @@
+using ExternalApi.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExternalApi
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var builder = WebApplication.CreateBuilder(args);
+      builder.Services.AddDbContext<ExternalApiDbContext>(options =>
+          options.UseSqlite(builder.Configuration.GetConnectionString("ExternalApiDbContext") ?? throw new InvalidOperationException("Connection string 'ExternalApiDbContext' not found.")));
+     
+      // Add services to the container.
+
+      builder.Services.AddControllers();
+      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+      builder.Services.AddEndpointsApiExplorer();
+      builder.Services.AddSwaggerGen();
+
+      var app = builder.Build();
+
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment())
+      {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseAuthorization();
+
+      app.MapControllers();
+
+      app.Run();
+    }
+  }
+}
